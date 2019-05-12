1f83295b60562a2b8e1b05dcf0a286d65559c49b
Kennedy Njeri <mistakenz123@gmail.com>
2019-05-12 23:54:42 +0300

    added tests for forms

--- budget/tests/test_forms.py
+++ budget/tests/test_forms.py
@@ -4,14 +4,7 @@ from budget.forms import ExpenseForm
 
 class TestForms(SimpleTestCase):
 
-    def test_expense_form_valid_data(self):
-        form = ExpenseForm(data={
-            'title': 'expense1',
-            'amount': 1000,
-            'category': 'development'
-        })
-
-        self.assertTrue(form.is_valid())
+    
 
 
     def test_expense_form_no_data(self):
