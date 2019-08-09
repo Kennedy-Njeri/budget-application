50b0ea410c0a85f8aea67cb6dc21fc898d573d43
Kennedy Njeri <mistakenz123@gmail.com>
2019-08-09 23:54:27 +0300

    code clean up

--- budget/views.py
+++ budget/views.py
@@ -53,8 +53,6 @@ def project_detail(request, project_slug):
     return HttpResponseRedirect(project_slug)
 
 
-
-
 class ProjectCreateView(CreateView):
     model = Project
     template_name = 'budget/add-project.html'
